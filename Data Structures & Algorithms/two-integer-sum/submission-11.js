18b4237c8fae8707fb3d25861361a676426298af
ishantgarg2332 <110192098+ishantgarg2332@users.noreply.github.com>
2026-07-06 16:44:11 +0530

    Add: two-integer-sum - submission-11

---
+++ Data Structures & Algorithms/two-integer-sum/submission-11.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let obj = {};
+    
+        for(let i = 0; i < nums.length; ++i) {
+            if(!obj[nums[i]]) {
+                obj[nums[i]] = [i];
+            }else {
+                obj[nums[i]].push(i);
+            }
+        }
+
+        for(let i = 0; i < nums.length; ++i) {
+            if(nums[i] === target - nums[i] && obj[nums[i]].length > 1) {
+                return obj[nums[i]];
+            }else if(obj[target - nums[i]] && (obj[nums[i]] !== obj[target-nums[i]])) {
+                return [...obj[nums[i]], ...obj[target - nums[i]]]
+            }
+        }
+
+        return [];
+    }
+}
