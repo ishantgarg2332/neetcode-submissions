0138799f9d860ca5191797c8374b911b6c6ff341
ishantgarg2332 <110192098+ishantgarg2332@users.noreply.github.com>
2026-07-09 08:57:54 +0530

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        const regex = /^[a-zA-Z0-9]$/;
+        let str = '';
+        for(let c of s) {
+            if(regex.test(c)) {
+                str += c;
+            }
+        }
+
+        str = str.toLowerCase();
+
+        let i = 0, j = str.length - 1;
+
+        while(i < j) {
+            if(str[i] !== str[j]) return false;
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
