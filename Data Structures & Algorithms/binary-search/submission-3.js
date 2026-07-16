3fc392a6c0b1240620f0b15624a1a35ea4159bdd
Ishant Garg <110192098+ishantgarg2332@users.noreply.github.com>
2026-07-16 12:21:06 +0530

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let low = 0, high = nums.length - 1;
+        while(low <= high) {
+            let mid = (low + high) >>> 1;
+            if(nums[mid] === target) return mid;
+            if(nums[mid] < target) {
+                low = mid + 1;
+            }else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
