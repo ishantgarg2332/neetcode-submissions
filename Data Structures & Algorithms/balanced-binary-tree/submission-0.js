39400539e38ed6021ef5801c682954894e2c0d3b
Ishant Garg <110192098+ishantgarg2332@users.noreply.github.com>
2026-07-31 16:32:58 +0530

    Add: balanced-binary-tree - submission-0

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-0.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    height(root) {
+        if(!root) return 0;
+
+        return 1 + Math.max(this.height(root.left), this.height(root.right));
+    }
+
+    isBalanced(root) {
+        if(!root) return true;
+
+        if(Math.abs(this.height(root.left) - this.height(root.right)) <= 1) {
+            return this.isBalanced(root.left) && this.isBalanced(root.right);
+        }
+
+        return false;
+
+    }
+}
